aa730d72b74c432814da2a4c0486fd3ea873d21c
suki <sukialiong@gmail.com>
2019-11-17 15:03:36 +1100

    mongo atlas connection initialized

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log("connected to the database")
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
